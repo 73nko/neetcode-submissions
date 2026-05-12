211095f714a91368f81f0166652a687b3a96dfae
73NKo <apramos89@gmail.com>
2026-05-12 17:51:31 +0200

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.ts
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums: number[]): boolean {
+        if(nums.length <=1) return false;
+        const seen = new Set<number>();
+
+        for (const n of nums) {
+            if (seen.has(n)) return true;
+
+            seen.add(n);
+        }
+
+        return false;
+    }
+}
