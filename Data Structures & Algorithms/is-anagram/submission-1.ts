e7ce9ad9bd5cafe0688648655a89a62dd6908df7
73NKo <apramos89@gmail.com>
2026-05-14 15:05:03 +0200

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.ts
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        if (s.length !== t.length) return false;
+        const counts = new Map<string, number>();
+        for (const c of s) counts.set(c, (counts.get(c) ?? 0) + 1);
+        for (const c of t) {
+            const n = (counts.get(c) ?? 0) - 1;
+            if (n < 0) return false;
+            counts.set(c, n);
+        }
+        return true;
+    }
+}
