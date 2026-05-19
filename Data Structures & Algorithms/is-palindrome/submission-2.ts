51eb3f7505675a59d6ca9d70cc486a65d3c309d1
73NKo <apramos89@gmail.com>
2026-05-19 16:57:14 +0200

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.ts
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+        let left = 0;
+  let right = s.length - 1;
+  while (left < right) {
+    while (left < right && !isAlphaNum(s[left]!)) left++;
+    while (left < right && !isAlphaNum(s[right]!)) right--;
+    if (s[left]!.toLowerCase() !== s[right]!.toLowerCase()) return false;
+    left++;
+    right--;
+  }
+  return true;
+    }
+}
+
+
+function isAlphaNum(c: string): boolean {
+  const code = c.charCodeAt(0);
+  return (
+    (code >= 48 && code <= 57) ||  // 0-9
+    (code >= 65 && code <= 90) ||  // A-Z
+    (code >= 97 && code <= 122)    // a-z
+  );
+}
